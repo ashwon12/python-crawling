e525f41bc93c1c5899a50c3a349c67a623873c26
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2022-08-05 05:00:09 +0900

    [Update] for문을 while문으로 변경

--- pythonProject/shopping.py
+++ pythonProject/shopping.py
@@ -67,11 +67,12 @@ def getSellerInfo(mall_List):
     global data
     result = []
     statusCode = 0
-    for idx in range(len(mall_List)):
+    idx = 0
+    while idx < len(mall_List) :
         mall = mall_List[idx]
         ''' Progress bar 값 변경 '''
         if (_CurrentProgress != None) and (_ProgressBar != None):
-            _CurrentProgress.set( ((idx/len(mall_List)) * 100)+10 )
+            _CurrentProgress.set(((idx / len(mall_List)) * 100) + 10)
             _ProgressBar.update()
 
         '''
@@ -93,7 +94,7 @@ def getSellerInfo(mall_List):
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
         }
         '''
-        #del sellerHeaders
+        # del sellerHeaders
         sellerHeaders = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
         }
@@ -106,32 +107,28 @@ def getSellerInfo(mall_List):
         status_chk_count = 0
         status_chk_flag = False
         while status_chk_flag != True:
-            if status_chk_count > 3:    # 해당 카운트가 3 이상이면 다음 mall 정보를 요청하기위해 while 문을 멈춘다.
+            if status_chk_count > 3:  # 해당 카운트가 3 이상이면 다음 mall 정보를 요청하기위해 while 문을 멈춘다.
                 break
             try:
-                time.sleep( 1 )   # request 하기 전에 0.5초의 딜레이를 준다. (503 에러 방지)
-                data = requests.get(mall, headers=sellerHeaders, params = params)     # DDoS 유발 가능성 존재. if Status == 503: sleep.until(ServerOpen)
-                print( "[SEND] {0}".format(mall))
-                status_chk_flag = True # request가 정상적으로 이루어졌을 경우, 이 값을 True로 변경
+                time.sleep(10)  # request 하기 전에 0.5초의 딜레이를 준다. (503 에러 방지)
+                data = requests.get(mall, headers=sellerHeaders,
+                                    params=params)  # DDoS 유발 가능성 존재. if Status == 503: sleep.until(ServerOpen)
+                print("[SEND] {0}".format(mall))
+                status_chk_flag = True  # request가 정상적으로 이루어졌을 경우, 이 값을 True로 변경
             except Exception as e:
-                status_chk_count += 1   # requests.get 이 정상적으로 수행되지 않았을 경우, 해당 카운트를 1 증가한다.
-                time.sleep( 30 )    # request 시 에러가 발생할 경우, 30초 대기...
+                status_chk_count += 1  # requests.get 이 정상적으로 수행되지 않았을 경우, 해당 카운트를 1 증가한다.
+                time.sleep(70)  # request 시 에러가 발생할 경우, 30초 대기...
                 continue
-        
-        if status_chk_flag == False: # 위 로직에서 status_chk_count가 3번 이상 발생했을 경우, 이 조건문을 수행함
-            time.sleep( 30 )    # request 시 에러가 발생할 경우, 30초 대기...
-            continue
+
         ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+        statusCode = data.status_code
+        if data.status_code != 200:
+            print(">> Occurd not 200: {0} ({1})".format(mall,data.status_code))  # 위 로직에서 status_chk_count가 3번 이상 발생했을 경우, 이 조건문을 수행함
+            time.sleep(60)  # request 시 에러가 발생할 경우, 30초 대기...
+            continue
 
         soup = BeautifulSoup(data.text, 'html.parser')
         body = soup.select_one('div.oSdeQo13Wd > div')
-
-        statusCode = data.status_code
-        if data.status_code != 200:
-            print( ">> Occurd not 200: {0} ({1})".format(mall, data.status_code)) # 위 로직에서 status_chk_count가 3번 이상 발생했을 경우, 이 조건문을 수행함
-            time.sleep( 30 )    # request 시 에러가 발생할 경우, 30초 대기...
-            continue
-
         seller_name = soup.select_one('div._3MuEQCqxSb > div._2i91yA8LnF > div._34hhYZytTs > div > strong').text
         seller_boss = body.select_one('div:nth-child(1) > div:nth-child(2) > div._2PXb_kpdRh').text
         temp = body.select_one('div:nth-child(1) > div:nth-child(3) > div._2PXb_kpdRh > div')
@@ -145,6 +142,8 @@ def getSellerInfo(mall_List):
             '고객센터': seller_call,
             '이메일': seller_mail
         }
-
+        idx = idx+1
         result.append(mall_info)
+
+    print("[FINISH]")
     return result, statusCode
